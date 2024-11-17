c7fdb31cb6c2fe0b70ffaf1ac79cc90484df615b
S1M0N3-777 <simoalbanese07@gmail.com>
2024-11-18 00:05:43 +0100

    JS, cambio colori non funzionante

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,22 @@
+function random(min, max) {
+    let n = Math.random() * (max - min) + min
+    n = parseInt(n)
+    console.log(n)
+    return n
+}
+
 let button = document.querySelector("#button")
+let circle1 = document.querySelector("#circle1")
+let circle2 = document.querySelector("#circle2")
+let circle3 = document.querySelector("#circle3")
+let check = true
+let inner = document.querySelector("#inner-text")
+let delay = random(1000,3000)
+let now
+
+button.addEventListener("click", function(){
+    now = Date.now()
+ inner.innerHTML = "Attenti il semaforo verde, stai attento!"
+ console.log(now)
+
+})
